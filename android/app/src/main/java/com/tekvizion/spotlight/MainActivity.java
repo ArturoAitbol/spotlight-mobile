9f540062ea2799b83f58cf14ce87b88d03a1d6b9
Oscar Gonzalez <oscar_sgc@Mac.local>
2022-10-19 12:49:58 -0400

    Initial Skeleton

---
+++ android/app/src/main/java/com/tekvizion/spotlight/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tekvizion.spotlight;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tekvizion.spotlight',
+  appName: 'tekvizion-spotlight',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
