322d2423daeb5827978c78ce2db0572e2fd6d8bd
Oscar Gonzalez <oscar_sgc@icloud.com>
2022-10-20 13:24:27 -0400

    Replaces blank app layout with tab app layout

---
+++ src/app/tabnav/tabnav-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabnavPage } from './tabnav.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabnavPage,
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: () => import('../dashboard/dashboard.module').then(m => m.DashboardPageModule)
+      },
+      {
+        path: 'settings',
+        loadChildren: () => import('../settings/settings.module').then(m => m.SettingsPageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/dashboard',
+        pathMatch: 'full'
+      },
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/dashboard',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabnavPageRoutingModule { }
