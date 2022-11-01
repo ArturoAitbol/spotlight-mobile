28b5ceae1fe7fb608edfed3aef81e384e891ad69
ariel <ariel_arc.95@hotmail.com>
2022-11-01 15:09:09 -0400

    Add environment variables for integration and production

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -5,12 +5,16 @@
 export const environment = {
   production: false,
   apiEndpoint: 'http://localhost:7071/v1.0',
-  UI_CLIENT_ID:"e643fc9d-b127-4883-8b80-2927df90e275",
+
+  // Azure Active Directory Application details
   AUTHORITY:"https://login.microsoftonline.com/e3a46007-31cb-4529-b8cc-1e59b97ebdbd",
+  UI_CLIENT_ID:"e643fc9d-b127-4883-8b80-2927df90e275",
+  API_CLIENT_ID: 'abb49487-0434-4a82-85fa-b9be4443d158',
+  API_SCOPE: 'tekvizion.access',
+  
+  // BASE URL
   REDIRECT_URL_AFTER_LOGIN:window.location.origin + '/login',
   REDIRECT_URL_AFTER_LOGOUT:window.location.origin + '/login',
-  API_CLIENT_ID: 'abb49487-0434-4a82-85fa-b9be4443d158',
-  API_SCOPE: 'tekvizion.access'
 };
 
 /*
