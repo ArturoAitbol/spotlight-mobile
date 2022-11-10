8274107d905615369a5254122fcd25b237a28773
ariel <ariel_arc.95@hotmail.com>
2022-11-10 12:32:23 -0400

    Add service mocks

--- app/src/test/components/utils/router.mock.ts
+++ app/src/test/components/utils/router.mock.ts
@@ -1,6 +1,30 @@
+import { NavigationEnd, RouterEvent } from "@angular/router";
+import { Observable } from "rxjs";
 
 export const ROUTER_MOCK = {
   navigate: (commands: string[]) => {
+    console.log(commands);
+  }
+};
+
+export class routerMock {
+
+  expectedEvent: NavigationEnd;
+
+  constructor(expectedEvent:NavigationEnd){
+    this.expectedEvent = expectedEvent;
+  }
+  
+
+  events = new Observable<RouterEvent>((observer) => {
+    observer.next(this.expectedEvent);
+    observer.complete();
+    return {
+        unsubscribe: () => {}
+    };
+  })
+
+  navigate(commands: string[]) {
     console.log(commands);
   }
 };
