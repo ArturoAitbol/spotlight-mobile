2e8c18f5467f9b5096cece40efd2815f7a8fcb43
Arturo-AB <arturo.amoroso@atombeamtech.com>
2022-11-11 16:20:48 -0400

    Timeout 90000 test home menu

--- tests/src/main/java/com/tekvizion/appium/android/AutomatedAndroidDevice.java
+++ tests/src/main/java/com/tekvizion/appium/android/AutomatedAndroidDevice.java
@@ -125,11 +125,11 @@ public class AutomatedAndroidDevice extends AutomatedMobileDevice implements Aut
         desiredCapabilities.add(this.platformName);
         desiredCapabilities.add(this.platformVersion);
         desiredCapabilities.add(this.udid);
-        desiredCapabilities.add(new DesiredCapability(DesiredCapabilityOption.APP_NAME, getAppPath()));
-        desiredCapabilities.add(new DesiredCapability(DesiredCapabilityOption.ANDROID_TIMEOUT, "150000"));
-        desiredCapabilities.add(new DesiredCapability(DesiredCapabilityOption.SERVER_TIMEOUT, "150000"));
-        desiredCapabilities.add(new DesiredCapability(DesiredCapabilityOption.APP_PACKAGE_WAIT_TIMEOUT, "150000"));
-        desiredCapabilities.add(new DesiredCapability(DesiredCapabilityOption.ADB_EXEC, "150000"));
+//        desiredCapabilities.add(new DesiredCapability(DesiredCapabilityOption.APP_NAME, getAppPath()));
+        desiredCapabilities.add(new DesiredCapability(DesiredCapabilityOption.ANDROID_TIMEOUT, "90000"));
+        desiredCapabilities.add(new DesiredCapability(DesiredCapabilityOption.SERVER_TIMEOUT, "90000"));
+        desiredCapabilities.add(new DesiredCapability(DesiredCapabilityOption.APP_PACKAGE_WAIT_TIMEOUT, "90000"));
+        desiredCapabilities.add(new DesiredCapability(DesiredCapabilityOption.ADB_EXEC, "90000"));
         desiredCapabilities.add(new DesiredCapability(DesiredCapabilityOption.NEW_COMMAND_TIMEOUT, Constants.DRIVER_SESSION_COMMAND_TIMEOUT));
         desiredCapabilities.add(new DesiredCapability(DesiredCapabilityOption.AUTO_ACCEPT_ALERTS, true));
         desiredCapabilities.add(new DesiredCapability(DesiredCapabilityOption.APP_WAIT, false));
