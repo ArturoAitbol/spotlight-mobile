cfd798ac272f0176659a2a1ee16700188381a0d1
Oscar Gonzalez <oscar_sgc@icloud.com>
2022-11-17 12:02:50 -0400

    Updates for release generation

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.tekvizion.spotlight',
-  appName: 'tekvizion-spotlight',
+  appName: 'spotlight-mobile',
   webDir: 'www',
   bundledWebRuntime: false
 };
