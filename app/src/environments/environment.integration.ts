4f2dacba56a3362a5fcbd99a7728fe031a96b292
ariel <ariel_arc.95@hotmail.com>
2022-11-21 18:05:47 -0400

    Fix: incorrect api endpoint for integration

--- app/src/environments/environment.integration.ts
+++ app/src/environments/environment.integration.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiEndpoint: 'https://tekvizion360-portal-fa.azurewebsites.net/v1.0',
+  apiEndpoint: 'https://tekvlicenseserverpocfunction.azurewebsites.net/v1.0',
 
   // Azure Active Directory Application details
   AUTHORITY:"https://login.microsoftonline.com/e3a46007-31cb-4529-b8cc-1e59b97ebdbd",
