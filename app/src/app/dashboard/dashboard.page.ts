8eb2ce461d6d2e628578f0ca11009dbcab6dea02
Oscar Gonzalez <oscar_sgc@icloud.com>
2022-11-29 18:07:20 -0400

    Fixes missing icons, updates views

--- app/src/app/dashboard/dashboard.page.ts
+++ app/src/app/dashboard/dashboard.page.ts
@@ -15,27 +15,27 @@ import { SubaccountService } from '../services/subaccount.service';
 })
 export class DashboardPage implements OnInit {
 
-  serviceName:string;
-  appName:string;
-  timelapse:string;
+  serviceName: string;
+  appName: string;
+  timelapse: string;
 
-  lastUpdate:string;
-  charts:any[] = [];
+  lastUpdate: string;
+  charts: any[] = [];
   notes: Note[] = [];
 
-  latestNote:Note;
-  previousNotes:number;
-  subaccountId:string = null;
+  latestNote: Note;
+  previousNotes: number;
+  subaccountId: string = null;
 
-  isChartsDataLoading:boolean = true;
+  isChartsDataLoading: boolean = true;
   isNoteDataLoading: boolean = true;
 
   constructor(private ctaasDashboardService: CtaasDashboardService,
-              private noteService: NoteService,
-              private subaccountService: SubaccountService,
-              private ionToastService: IonToastService,
-              private actionSheetCtrl: ActionSheetController) {}
-  
+    private noteService: NoteService,
+    private subaccountService: SubaccountService,
+    private ionToastService: IonToastService,
+    private actionSheetCtrl: ActionSheetController) { }
+
   ngOnInit(): void {
     this.serviceName = 'SpotLight';
     this.appName = 'Microsoft Teams';
@@ -43,21 +43,21 @@ export class DashboardPage implements OnInit {
     this.fetchData();
   }
 
-  fetchData(event?:any){
-    this.subaccountService.getSubAccountList().subscribe((res)=>{
-      if(res.subaccounts.length>0){
+  fetchData(event?: any) {
+    this.subaccountService.getSubAccountList().subscribe((res) => {
+      if (res.subaccounts.length > 0) {
         this.subaccountService.setSubAccount(res.subaccounts[0]);
         this.subaccountId = this.subaccountService.getSubAccount().id;
         this.fetchCtaasDashboard(event);
         this.fetchNotes();
-      }else{
-        this.isChartsDataLoading=false;
-        this.isNoteDataLoading=false;
+      } else {
+        this.isChartsDataLoading = false;
+        this.isNoteDataLoading = false;
       }
-    },(err)=>{
+    }, (err) => {
       console.error(err);
-      this.isChartsDataLoading=false;
-      this.isNoteDataLoading=false;
+      this.isChartsDataLoading = false;
+      this.isNoteDataLoading = false;
     });
   }
 
@@ -65,25 +65,25 @@ export class DashboardPage implements OnInit {
     this.fetchData(event);
   };
 
-   fetchNotes(){
+  fetchNotes() {
     this.isNoteDataLoading = true;
     this.notes = [];
     this.latestNote = null;
     this.previousNotes = null;
-    this.noteService.getNoteList(this.subaccountId,'Open').subscribe((res:any)=>{
-      if(res!=null && res.notes.length>0){
+    this.noteService.getNoteList(this.subaccountId, 'Open').subscribe((res: any) => {
+      if (res != null && res.notes.length > 0) {
         this.notes = res.notes;
-        this.previousNotes = this.notes.length-1;
+        this.previousNotes = this.notes.length - 1;
         this.latestNote = this.notes[0];
       }
-      this.isNoteDataLoading=false;
-    },(err)=>{
+      this.isNoteDataLoading = false;
+    }, (err) => {
       console.error(err);
-      this.isNoteDataLoading=false;
+      this.isNoteDataLoading = false;
     });
   }
 
-   async deleteNote(){
+  async deleteNote() {
 
     const actionSheet = await this.actionSheetCtrl.create({
       header: 'Are you sure you want to delete this note?',
@@ -103,39 +103,39 @@ export class DashboardPage implements OnInit {
 
     const { role } = await actionSheet.onWillDismiss();
 
-    if(role === 'destructive'){
-      this.noteService.deleteNote(this.latestNote.id).subscribe((res)=>{
+    if (role === 'destructive') {
+      this.noteService.deleteNote(this.latestNote.id).subscribe((res) => {
         this.ionToastService.presentToast('Note deleted successfully!');
         this.fetchNotes();
-      },(err)=>{
+      }, (err) => {
         console.error(err);
-        this.ionToastService.presentToast("Error deleting a note","Error");
+        this.ionToastService.presentToast("Error deleting a note", "Error");
       })
     }
-   }
+  }
 
-   fetchCtaasDashboard(event?: any){
+  fetchCtaasDashboard(event?: any) {
     this.isChartsDataLoading = true;
     this.charts = [];
 
     const requests: Observable<any>[] = [];
-    for(const key in ReportType){
+    for (const key in ReportType) {
       const reportType: string = ReportType[key];
-      requests.push(this.ctaasDashboardService.getCtaasDashboardDetails(this.subaccountId,reportType));
+      requests.push(this.ctaasDashboardService.getCtaasDashboardDetails(this.subaccountId, reportType));
     }
 
-    forkJoin(requests).subscribe((res: [{ response?:string, error?:string }])=>{
-      if(res){
-        this.charts = [...res].map((e: {response:any})=> e.response ? e.response : e);
+    forkJoin(requests).subscribe((res: [{ response?: string, error?: string }]) => {
+      if (res) {
+        this.charts = [...res].map((e: { response: any }) => e.response ? e.response : e);
         this.lastUpdate = this.charts[0].lastUpdatedTS ? this.charts[0].lastUpdatedTS : null;
       }
-      if(event) event.target.complete();
+      if (event) event.target.complete();
       this.isChartsDataLoading = false;
-    },(e)=>{
+    }, (e) => {
       console.error('Error loading dashboard reports ', e.error);
       this.isChartsDataLoading = false;
       this.ionToastService.presentToast('Error loading dashboard, please connect tekVizion admin', 'Ok');
-      if(event) event.target.complete();
+      if (event) event.target.complete();
     })
   }
 
