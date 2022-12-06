665cfb0ba1ac5f7da73454b08413e251933a0644
Oscar Gonzalez <oscar_sgc@icloud.com>
2022-12-06 15:46:02 -0400

    Adds message for no internet connection

--- app/src/app/dashboard/dashboard.page.ts
+++ app/src/app/dashboard/dashboard.page.ts
@@ -13,39 +13,39 @@ import { SubaccountService } from '../services/subaccount.service';
 })
 export class DashboardPage implements OnInit {
 
-  serviceName:string;
-  appName:string;
+  serviceName: string;
+  appName: string;
 
-  charts:any[] = [];
-  
-  subaccountId:string = null;
+  charts: any[] = [];
 
-  isChartsDataLoading:boolean = true;
+  subaccountId: string = null;
+
+  isChartsDataLoading: boolean = true;
 
   constructor(private ctaasDashboardService: CtaasDashboardService,
-              private subaccountService: SubaccountService,
-              private ionToastService: IonToastService,
-              private dashboardService: DashboardService) {}
-  
+    private subaccountService: SubaccountService,
+    private ionToastService: IonToastService,
+    private dashboardService: DashboardService) { }
+
   ngOnInit(): void {
     this.serviceName = 'Spotlight';
     this.appName = 'Microsoft Teams';
     this.fetchData();
   }
 
-  fetchData(event?:any){
-    this.subaccountService.getSubAccountList().subscribe((res)=>{
-      if(res.subaccounts.length>0){
+  fetchData(event?: any) {
+    this.subaccountService.getSubAccountList().subscribe((res) => {
+      if (res.subaccounts.length > 0) {
         this.subaccountService.setSubAccount(res.subaccounts[0]);
         this.subaccountId = this.subaccountService.getSubAccount().id;
         this.fetchCtaasDashboard(event);
-      }else{
-        this.isChartsDataLoading=false;
-      }
+      } else
+        this.isChartsDataLoading = false;
     }, (err) => {
-      console.error(err);
-      this.isChartsDataLoading=false;
-      if (event) event.target.complete();
+      // console.error(err);
+      this.isChartsDataLoading = false;
+      if (event)
+        event.target.complete();
     });
   }
 
@@ -53,7 +53,7 @@ export class DashboardPage implements OnInit {
     this.fetchData(event);
   };
 
-  fetchCtaasDashboard(event?: any){
+  fetchCtaasDashboard(event?: any) {
     this.isChartsDataLoading = true;
     this.charts = [];
 
@@ -63,24 +63,26 @@ export class DashboardPage implements OnInit {
       requests.push(this.ctaasDashboardService.getCtaasDashboardDetails(this.subaccountId, reportType));
     }
 
-    forkJoin(requests).subscribe((res: [{ response?:string, error?:string }])=>{
-      if(res){
+    forkJoin(requests).subscribe((res: [{ response?: string, error?: string }]) => {
+      if (res) {
         this.charts = [...res].filter((e: any) => !e.error).map((e: { response: string }) => e.response);
-        if(this.charts.length>0){
-          let reports = this.charts.map((chart:any)=>{
+        if (this.charts.length > 0) {
+          let reports = this.charts.map((chart: any) => {
             // Destructure the chart object to save only timestampId and type attributes
             return (({ timestampId, reportType }) => ({ timestampId, reportType }))(chart);
           });
           this.dashboardService.setReports(reports);
         }
       }
-      if (event) event.target.complete();
+      if (event)
+        event.target.complete();
       this.isChartsDataLoading = false;
     }, (e) => {
       console.error('Error loading dashboard reports ', e.error);
       this.isChartsDataLoading = false;
-      this.ionToastService.presentToast('Error loading dashboard, please connect tekVizion admin', 'Ok');
-      if (event) event.target.complete();
+      this.ionToastService.presentToast('Error loading dashboard, please contact tekVizion admin', 'Ok');
+      if (event)
+        event.target.complete();
     })
   }
 
--- app/src/app/services/ion-toast.service.ts
+++ app/src/app/services/ion-toast.service.ts
@@ -2,23 +2,23 @@ import { Injectable } from "@angular/core";
 import { ToastController } from "@ionic/angular";
 
 @Injectable({
-    providedIn:'root'
+  providedIn: 'root'
 })
 export class IonToastService {
 
-    constructor(private toastController: ToastController){}
+  constructor(private toastController: ToastController) { }
 
-    async presentToast(message:string,dismissButtonText?:string,position?: 'top' | 'middle' | 'bottom',duration?: number) {
-        const toast = await this.toastController.create({
-          message: message,
-          cssClass: 'custom-toast',
-          duration: duration ? duration : 2000,
-          position: position ? position : 'top',
-          buttons: dismissButtonText ? [ {
-              text: dismissButtonText,
-              role: 'cancel'
-            }]:null
-        });
-        await toast.present();
-      }
+  async presentToast(message: string, dismissButtonText?: string, position?: 'top' | 'middle' | 'bottom', duration?: number) {
+    const toast = await this.toastController.create({
+      message: message,
+      cssClass: 'custom-toast',
+      duration: duration ? duration : 2500,
+      position: position ? position : 'top',
+      buttons: dismissButtonText ? [{
+        text: dismissButtonText,
+        role: 'cancel'
+      }] : null
+    });
+    await toast.present();
+  }
 }
