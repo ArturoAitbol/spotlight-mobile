6280525689b2898e00b82a2b61cec70d8e647cc6
ArielRasguido <ariel_arc.95@hotmail.com>
2023-01-25 16:43:30 -0400

    Add controls to limit the access of users with no spotlight service

--- app/src/app/security/role.guard.ts
+++ app/src/app/security/role.guard.ts
@@ -3,6 +3,8 @@ import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
 import { MsalService } from '@azure/msal-angular';
 import { permissions } from './role-permissions';
 import { IonToastService } from '../services/ion-toast.service';
+import { NoPermissionMessage } from '../model/no-permission-message.model';
+import { NoPermissionErrorService } from '../services/no-permission-error.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +13,20 @@ export class RoleGuard implements CanActivate {
 
   constructor(
     private msalService: MsalService,
-    private route: Router,
-    private ionToastService: IonToastService) {
+    private router: Router,
+    private ionToastService: IonToastService,
+    private noPermissionErrorService: NoPermissionErrorService) {
   }
 
   canActivate(route: ActivatedRouteSnapshot): boolean {
     const idTokenClaims: any = this.msalService.instance.getActiveAccount().idTokenClaims;
     if(!idTokenClaims.roles || !this.rolesExist(idTokenClaims.roles)){
       this.ionToastService.presentToast('Role is missing', 'NOT AUTHORIZED');
-      this.route.navigate(['/login/no-permission']);
+      const title = 'You do not have access as role is missing';
+      const message: NoPermissionMessage[] = [{type:'static',content:'Please ensure that your account is assigned to a proper role and then sign-out and sign-in again.'}];
+      this.noPermissionErrorService.setError(title,message);
+      this.noPermissionErrorService.announceErrorMessageUpdate();
+      this.router.navigate(['/login/no-permission']);
       return false;
     }
     if (!this.isAuthorized(route, idTokenClaims.roles)) {
@@ -28,7 +35,7 @@ export class RoleGuard implements CanActivate {
     }
     return true;
   }
-  
+
   private isAuthorized(route: ActivatedRouteSnapshot, roles: string[]): boolean{
     const path = route.url[0].path;
     const pathsMatch = roles.findIndex(role =>permissions[role] ? permissions[role].paths.indexOf(path)!==-1 : false);
@@ -38,7 +45,7 @@ export class RoleGuard implements CanActivate {
   private rolesExist(roles:string[]):boolean{
     for (let i = 0; i < roles.length; i++) {
       if(permissions[roles[i]])
-        return true; 
+        return true;
     }
     return false;
   }
--- app/src/app/shared/shared.module.ts
+++ app/src/app/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccessControlDirective } from '../directives/access-control.directive';
+import { CheckHtmlPipe } from '../helpers/checkHtml.pipe';
 
 
 @NgModule({
-  declarations: [AccessControlDirective],
+  declarations: [AccessControlDirective,CheckHtmlPipe],
   imports: [
     CommonModule
   ],
-  exports: [AccessControlDirective]
+  exports: [AccessControlDirective,CheckHtmlPipe]
 })
 export class SharedModule { }
