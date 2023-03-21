f395a239e2a6509bd3cc3949e05580f48c9ee0ee
ArturoAitbol <aamoroso@aitbol.com>
2023-03-21 09:57:57 -0400

    Improve login test on Android

--- tests/src/main/java/com/tekvizion/pageObjects/android/HomePageAndroid.java
+++ tests/src/main/java/com/tekvizion/pageObjects/android/HomePageAndroid.java
@@ -2,12 +2,17 @@ package com.tekvizion.pageObjects.android;
 
 import com.tekvizion.pageObjects.ios.Login;
 import com.tekvizion.utils.AndroidActions;
+import io.appium.java_client.android.Activity;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HomePageAndroid extends AndroidActions {
     AndroidDriver driver;
@@ -23,7 +28,17 @@ public class HomePageAndroid extends AndroidActions {
         PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     }
     public LoginAndroid goToLoginForm(){
-        click(loginButton);
+        try {
+            waitElement(loginButton, 180);
+            click(loginButton);
+        } catch (Exception e) {
+            System.out.println("Login button wasn't displayed initially!");
+            System.out.println(e.toString());
+            Activity activity = new Activity("com.tekvizion.spotlight", "com.tekvizion.spotlight.MainActivity");
+            driver.startActivity(activity);
+            waitElement(loginButton, 180);
+            click(loginButton);
+        }
         return new LoginAndroid(driver);
     }
 }
