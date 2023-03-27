990f7a9150b3c6969d75cf5409302579fb940225
Oscar Gonzalez <oscar_sgc@icloud.com>
2023-03-27 17:04:11 -0400

    Fix workflows

--- app/karma.conf.js
+++ app/karma.conf.js
@@ -3,43 +3,63 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+      require("karma-spec-reporter"),
+      require("karma-junit-reporter"),
+      require("karma-json-reporter"),
+      require("@angular-devkit/build-angular/plugins/karma"),
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+    reporters: ["progress", "kjhtml", "spec", "coverage", "junit"],
+    junitReporter: {
+      outputDir: require("path").join(__dirname, "./test-results"), // results will be saved as $outputDir/$browserName.xml
+      outputFile: "tests-results.xml", // if included, results will be saved as $outputDir/$browserName/$outputFile
+      suite: "Unit Tests", // suite will become the package name attribute in xml testsuite element
+      useBrowserName: false, // add browser name to report and classes names
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './test-results/karma-coverage'),
-      subdir: '.',
+      dir: require("path").join(__dirname, "./test-results/karma-coverage"),
+      subdir: ".",
+      include: "src/**/!(*.spec).ts",
+      exclude: "src/main.ts",
       reporters: [
-        { type: 'html' },
-        { type: 'text-summary' },
-        { type: 'json-summary', subdir: '../', file: 'coverage-summary.json' }
-      ]
+        { type: "html" },
+        { type: "lcovonly" },
+        { type: "text-summary", subdir: "../" },
+        { type: "json-summary", subdir: "../", file: "coverage-summary.json" },
+      ],
+      fixWebpackSourcePaths: true,
     },
-    reporters: ['progress', 'kjhtml', 'coverage'],
+    specReporter: {
+      maxLogLines: 5, // limit number of lines logged per test
+      suppressSummary: true, // do not print summary
+      suppressErrorSummary: true, // do not print error summary
+      suppressFailed: false, // do not print information about failed tests
+      suppressPassed: false, // do not print information about passed tests
+      suppressSkipped: true, // do not print information about skipped tests
+      showBrowser: false, // print the browser for each spec
+      showSpecTiming: false, // print the time elapsed for each spec
+      failFast: false, // test would finish with error when a first fail occurs
+      prefixes: {
+        success: "    OK: ", // override prefix for passed tests, default is '✓ '
+        failure: "FAILED: ", // override prefix for failed tests, default is '✗ '
+        skipped: "SKIPPED: ", // override prefix for skipped tests, default is '- '
+      },
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['ChromeHeadless'],
+    browsers: ["ChromeHeadless"],
     singleRun: true,
-    restartOnFileChange: true
+    restartOnFileChange: true,
   });
 };
